5f81de9f4d163ed28a4256b9d740516abaeb6a03
venzard <vasilij-ch@ya.ru>
2022-12-08 22:23:47 +0300

    upd search

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,15 @@ import Footer from "./components/Footer/footer"
 
 import Catalog from "./pages/Catalog";
 import Home from "./pages/Home";
-import Search from "./components/Search/search";
+//import Search from "./components/Search/search";
 
 const smiles =["^-^", "=)","0_o",":-)", "@_@", "^-0", "-_-"]
 const App  =()=>{  
     const [user, setUser] = useState(localStorage.getItem("user8"));
     return (
         <div className="container">
-            <Header user={user} setUser={setUser}/>
+            <Header user={user} setUser={setUser} products={products}/>
             <main>
-                <Search data={products}/>
                 {user ? <Catalog data={products}/>:<Home data={smiles}/>}
             </main>
             <Footer/>
