a87aae8bf4bacd9eed15a6527dd915fa45a88c21
venzard <vasilij-ch@ya.ru>
2022-12-13 21:35:52 +0300

    create Auth

--- src/components/Modal/Login.jsx
+++ src/components/Modal/Login.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-export default ({change, api, close}) => {
+export default ({change, api, close, setToken}) => {
     const [inp1, setInp1] = useState("");
     const [inp2, setInp2] = useState("");
 
@@ -14,6 +14,9 @@ export default ({change, api, close}) => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
+                localStorage.setItem("user8", data.data.name);
+                localStorage.setItem("token8", data.token);
+                setToken(data.token);
                 setInp1("");
                 setInp2("");
                 close(false);
