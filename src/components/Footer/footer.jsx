46a798c7c5276297150c5886ec7143775ed82059
venzard <vasilij-ch@ya.ru>
2022-12-20 00:26:01 +0300

    add part homepage

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,9 +5,59 @@ export default () => {
     const year = new Date().getFullYear();
     return (
         <footer>
-            <span className="footer__copy">©{year}</span>
-            <span className="footer__text">Сайт разработан 
-            с использованием <br/>библиотеки React</span>
+            <div className="footer__wrapper">
+                <div className="footer__logo">
+                    <a href="#" className="footer__logo-link">DoGFooD</a>
+                </div>
+
+                <div className="footer__nav nav1">
+                    <ul className="footer__list">
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Каталог</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Акции</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Новости</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Отзывы</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="footer__nav nav2">
+                    <ul className="footer__list">
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Оплата и доставка</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Часто спрашивают</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Обратная связь</a>
+                        </li>
+                        <li className="footer__item">
+                            <a href="#" className="footer__link">Контакты</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="social">
+                    <ul className="soc__list">
+                        <li className="soc__item" >
+                            <h2>Мы на связи</h2>
+                        </li>
+                        <li className="soc__item">
+                            <h3>8 (999) 00-00-00</h3>
+                        </li>
+                        <li className="soc__item">
+                            <h3>dogfood@gmail.com</h3>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </footer>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,30 @@
 import React from "react";
 import Card from "../components/Card";
+import "./style.css"
 
 
 export default ({data})=>{
     return(
         <>
-        <h1>Главная страница</h1>
-        <div className="cards">
+        <div className="hero">
+            <div className="hero-title">
+                <h1>Крафтовые<br/>лакомства для<br/>собак</h1>
+                <p>Всегда свежие лакомства ручной<br/>работы с доставкой по России и миру</p>
+                <button className="toCatalog">Каталог > </button> 
+            </div>
+        </div>
+        <div className="cards wrapper">
+                <img  className="ad ad1" src="/img/adv-1.jpg" alt="ad block 1"/>
                 {data.map((el,i) => <Card key={"card_" + i} text={el} like={(i+1) % 2 === 0}/>)}
+                <div className="ad-double">
+                    <img  className="ad ad2" src="/img/adv-2.jpg" alt="ad block 2"/>
+                    <img  className="ad ad2a" src="/img/adv-2a.jpg" alt="ad block 2a"/>
+                </div> 
+                {data.map((el,i) => <Card key={"card_" + i} text={el} like={(i+1) % 2 === 0}/>)}
+                <div className="ad-double">
+                    <img  className="ad ad3" src="/img/adv-3.jpg" alt="ad block 3"/>
+                    <img  className="ad ad3a" src="/img/adv-3a.jpg" alt="ad block 3a"/>
+                </div> 
         </div>
         </>
     )
