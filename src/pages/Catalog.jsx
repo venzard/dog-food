6d4e52cc98e80fef0ec6b4a36a5e3a404777eacb
venzard <vasilij-ch@ya.ru>
2023-01-24 14:24:21 +0300

    add pagination

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -3,17 +3,20 @@ import Card from "../components/Card";
 import {Link} from "react-router-dom";
 import {EmojiFrown} from "react-bootstrap-icons";
 import Ctx from "../Ctx";
+import usePagination from "../hooks/usePagination";
+import Pagination from "../components/Pagination";
 
 export default ({data}) => {
     const {visibleGoods, user, PATH} = useContext(Ctx);
-    // data => visibleGoods
+    const paginate = usePagination(visibleGoods, 12);
     return <>
         {user && <>
             {visibleGoods.length > 0 
                 ? <>
                     <h1>Каталог товаров</h1>
+                    <Pagination hook={paginate}/>
                     <div className="cards">
-                        {visibleGoods.map((el, i) => <Link to={`/catalog/${el._id}`} key={el._id}>
+                        {paginate.setPageData().map((el, i) => <Link to={`/catalog/${el._id}`} key={el._id}>
                             <Card key={"card_" + i} {...el}/>
                         </Link>)}
                     </div>
